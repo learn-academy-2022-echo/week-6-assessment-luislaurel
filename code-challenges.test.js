145404bcba195dc99cafcc3f9c1e821d06d687b4
luislaurel <luislaurel133@gmail.com>
2022-09-19 09:26:55 -0600

    Final ASSessment submitted

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,8 +1,9 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
-
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
+
+
 
 // Use test driven development to complete the following questions
 // Add appropriate dependencies: $ yarn add jest
@@ -14,15 +15,68 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
+// Psuedo
+// describe will take arrayOfObjects func
+// it "takes in an array of objects and returns an array with a sentence about each person with their name capitalized."\
+// to Equal sentence with each name capitalized
+
+
 const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
 ]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+
+describe("arrayOfObjects", ()=> {
+  it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized.", ()=> {
+    expect(arrayOfObjects(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
+
+
+ FAIL  ./code-challenges.test.js
+  arrayOfObjects
+    ✕ takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (2 ms)
+
+  ● arrayOfObjects › takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+
+    ReferenceError: arrayOfObjects is not defined
+
+Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
 // b) Create the function that makes the test pass.
 
+// Psuedo code
+// Create function named arrayofObjects
+// it will take in variable people
+// map over array to return a array
+// .split(" ") will seperate the names
+// need to map over this value because you cannot use .charAt(0).toUpperCase(0) following it, which is needed to make first letter uppercase.
+// concat sliced value, followed by string interpolation to give us the full sentence
+// will return an array with a sentence about each person, name capitalized
+// output  ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+// Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
+
+
+const arrayOfObjects = (array) => {
+ return array.map(value => value.name.split(" ")
+  .map(value => value.charAt(0).toUpperCase() + value.slice(1)).join(" ") + ` is ${value.occupation}.`
+  )
+}
+
+ PASS  ./code-challenges.test.js
+  arrayOfObjects
+    ✓ takes in an array of objects and returns an array with a sentence about each person with their name capitalized. (2 ms)
+
+
+
+    
+    const arrayOfObjects = (array) => {
+ return array.map(value => value.name.split(" ").charAt(0).toUpperCase()
+
+//     }
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
@@ -32,7 +86,45 @@ const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+
+describe("remainderArray", ()=> {
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", ()=> {
+    expect(remainderArray(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(remainderArray(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+
+ FAIL  ./code-challenges.test.js
+  remainderArray
+    ✕ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. (2 ms)
+
+  ● remainderArray › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+
+    ReferenceError: remainderArray is not defined
+
 // b) Create the function that makes the test pass.
+
+
+// Psuedo code
+// function named remainderArray 
+// takes in hodgepodge1,2
+// create a variable to store initial value
+// Need to return an array with numbers only, .filter() will return a shortened array and typeof to look for numbers
+// return variable and map over to get the remainder of all those numbers % 3
+// returns an array with remainders divided by 3
+// expect [ 2, 0, -1, 0 ], [ 2, 1, -1 ]
+
+
+
+
+const remainderArray = (array) => {
+ numbersOnlyArray = array.filter(value => typeof value === 'number' )
+return numbersOnlyArray.map(value => value % 3)
+ }
+
+  PASS  ./code-challenges.test.js
+  remainderArray
+    ✓ takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3. (3 ms)
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
@@ -43,4 +135,40 @@ const cubeAndSum1 = [2, 3, 4]
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe("sumOfAllCubed", () => {
+  it(" takes in an array of numbers and returns the sum of all the numbers cubed", () => {
+    expect(sumOfAllCubed(cubeAndSum1)).toEqual(99)
+    expect(sumOfAllCubed(cubeAndSum2)).toEqual(1125)
+  })
+})
+
+ FAIL  ./code-challenges.test.js
+  sumOfAllCubed
+    ✕  takes in an array of numbers and returns the sum of all the numbers cubed (1 ms)
+
+  ● sumOfAllCubed ›  takes in an array of numbers and returns the sum of all the numbers cubed
+
+    ReferenceError: sumOfAllCubed is not defined
+
 // b) Create the function that makes the test pass.
+
+// Psuedo
+// create function named sumOfAll
+// takes in cubeAndSum1,2
+// created variable to store 3
+// Map over array
+//  use Math.pow() to cube it. passed value, exponent into params. ex first in the array. value (2) would be raised to the power of exponent (3)
+// created new variable for the sum. takes each index and adds them up.
+// expect 99 , 1125
+
+
+
+const sumOfAllCubed = (array) => {
+let exponent = 3
+cubed = array.map(value => Math.pow(value, exponent))
+return sumThemAll = cubed[0] + cubed[1] + cubed[2]
+}
+
+ PASS  ./code-challenges.test.js
+  sumOfAllCubed
+    ✓  takes in an array of numbers and returns the sum of all the numbers cubed (2 ms)
--- interview-questions.md
+++ interview-questions.md
@@ -8,36 +8,84 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
-
+Your answer: Well I believe the easiest fix would be to do a db:rollback, this is what I've used in my experience working with rails. If the error has been identified right away. But If it hasn't I think we would have to use migrations, I'm not too sure exactly what that would be though. The foreign key would be student.
+   
 Researched answer:
+
+So upon further research you would have to do rails db:rollback to undo the latest migration. You would then have to update the schema file. The name of the foreign key would be the cohort_id and the foreign key would be on the student model. Because Student belongs_to cohort.
+
 
 2. Which RESTful routes must always be passed params? Why?
 
-Your answer:
+Your answer: The ones I know require are destroy,show,update and edit. They need an id in order perform what they are doing. I can't just destroy nothing.
 
-Researched answer:
+Researched answer: 
+
+Destroy, Show, Update, Edit are the REStful routes that require params. This is because there is a specific piece of data that must be identified to modify. For example if I wanted to Destroy something that method would require an ID because it needs to be told what to delete. 
 
 3. Name three rails generator commands. What is created by each?
 
 Your answer:
 
+rails generate model Fish color:string, age:integer - This would create a model with the name of Fish, and columns with the name of color, age. The data type for each column is what follows after the colon. 
+
+
+rails generate controller - This would create a controller for us. All of the methods would end up being added here.
+
+rails generate rspec:install - This would add the needed dependencies for rspec testing. We would then see a rspec file with files for testing.
+
+
 Researched answer:
+There are many generate commands, a few that I have worked with are generate controller, rspec:install and model.
+ These generate commands are apart of what make working with rails much more convenient. We don't have to the extra work of creating the files that will be added when these commands are ran. 
+
+rails generate model Fish color:string, age:integer - This would create a model with the name of Fish, and columns with the name of color, age. The data type for each column is what follows after the colon. 
+
+
+rails generate controller - This would create a controller for us. All of the methods would end up being added here.
+
+rails generate rspec:install - This would add the needed dependencies for rspec testing. We would then see a rspec file with files for testing.
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
 action: "GET" location: /students
+Index - Used to get all the data being stored in students
 
 action: "POST" location: /students
 
+Create - this is how a new addition would be added to the database
+
+
 action: "GET" location: /students/new
+
+New - HTML form for creating a new student.
 
 action: "GET" location: /students/2
 
+Show - This shows a specific item which in this case would have an id of 2.
+
 action: "GET" location: /students/2/edit
+
+Edit - This would be an HTML form. This is how you would update an item. This items ID is 2.
 
 action: "PATCH" location: /students/2
 
+Update - How you would update an existing item. 
+
 action: "DELETE" location: /students/2
 
+Destroy - You would remove item with ID 2 from the database.
+
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+
+Story 1 - User is able to see title of task
+Story 2 - User can see a description of task
+Story 3 - User can create a new task
+Story 4 - User can update task
+Story 5 - User can mark each task complete/incomplete
+Story 6 - User can choose to delete task
+Story 7 - User is able to see each task with its own page
+Story 8 - User can add his or someone else's name for each task
+Story 9 - User can set a date for each task
+Story 10 - User can set a time for each task
